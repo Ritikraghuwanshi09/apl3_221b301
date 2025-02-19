8cb20b9e930ec94e2fe16e3011d5778df711aa6c
221B301 <221B301@juet.co.in>
2025-02-19 14:33:56 +0530

    PR2

---
+++ LE3/PR2/Test.java
@@ -0,0 +1,18 @@
+interface Testable {
+    void display();
+}
+
+class Test implements Testable {
+    @Override
+    public void display() {
+        System.out.println("Display method from Test class.");
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.display();
+    }
+}
+
+abstract class AbsTest implements Testable {
+}
